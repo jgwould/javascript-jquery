0636cd65620f2249ab953b6b5e5324eda555d3ed
John Garland Wood <woodjohngarland@gmail.com>
2017-01-11 10:57:46 +0700

    edits to HTML and CSS, getting started with js

---
+++ site.js
@@ -0,0 +1,12 @@
+
+function gridsize(){
+do{
+
+var gridsize = parseInt(prompt("How large would you like your canvas? (Please select a size between 16 and 100)"));
+
+}
+while(gridsize(isNan) || gridsize > 100 || gridsize < 16);
+}
+
+
+
