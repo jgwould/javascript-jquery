5a4fc19dc052c397065b85b288e89c9c065fe7cd
John Garland Wood <woodjohngarland@gmail.com>
2017-01-13 11:00:26 +0700

    edits. working with input

--- site.js
+++ site.js
@@ -1,12 +1,26 @@
 
-function gridsize(){
-do{
+$('#startbutton').click(function(){
+	var input = prompt("How large would you like your canvas? (Please select a size between 16 and 100)");
+	if (input < 100 && input > 16){
+		return input;
+	}
+	else {
+		promptUser();
+	}
+}
 
-var gridsize = parseInt(prompt("How large would you like your canvas? (Please select a size between 16 and 100)"));
+var gridsize = input*input
+
+$(document).ready(function(){
+	for (var x = 0; x =< input; x ++) {
+		for ( var y <= 0; y =< gridsize; y++) {
+			var boxes = $("<div class ='boxes'></div>");
+			boxes.appendTo('.warehouse');
+			
+		}
+	}
+
 
 }
-while(gridsize(isNan) || gridsize > 100 || gridsize < 16);
-}
-
 
 
